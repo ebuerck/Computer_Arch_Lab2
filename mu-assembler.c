0f5014b0291b9c50c779c8dafa3a54e0a0f05aa1
Ethan Buerck <ebuerck@gmail.com>
2022-09-29 10:02:14 -0500

    Added some basic arg checking

---
+++ mu-assembler.c
@@ -0,0 +1,14 @@
+#include "mu-assembler.h"
+
+
+
+/***************************************************************/
+/* Main function. */
+/***************************************************************/
+int main(int argc, char *argv[]){
+   if(argc != 3)
+   {
+      printf("ERROR: No included input file\n");
+      return -1;
+   }
+}
