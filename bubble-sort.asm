668cf3cd2b4afdf5b0742ecd4b68ddf6ef0fe3af
Bobby Love <lovebobbyandrew@outlook.com>
2022-10-17 10:16:05 -0500

    created fibonacci.asm

--- bubble-sort.asm
+++ bubble-sort.asm
@@ -5,16 +5,14 @@ ARRAY: .WORD 5, 3, 6, 8, 9, 1, 4, 7, 2, 10
 BUBBLE:
     LI      $T0,        0                   # I = 0
     LI      $T1,        0                   # J = 0
+    LI      $S0,        10                  # SIZE = 10
 OUTERFOR:
-    BLE     $S0,        $T0,    END         # IF A <= I, EXIT OUTERFOR.
-    
-
+    BLE     $S0,        $T0,    END         # IF SIZE <= I, EXIT OUTERFOR.
     ADDI    $T0,        $T0,    1           # I = I + 1
     LI      $T1,        0                   # J = 0
     J       INNERFOR
 INNERFOR:
-    BLE     $S1,        $T1,    OUTERFOR    $ IF B <= J, RESTART OUTERFOR.
-
+    BLE     $S0,        $T1,    OUTERFOR    $ IF SIZE <= J, RESTART OUTERFOR.
 
     ADDI    $T1,        $T1,    1           # J = J + 1
     J       INNERFOR
