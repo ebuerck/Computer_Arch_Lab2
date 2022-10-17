0f77fb55f27b8898652cc6ea9f7701a8bf5dd6c7
Bobby Love <lovebobbyandrew@outlook.com>
2022-10-17 12:12:17 -0500

    improved spacing

--- bubble-sort.asm
+++ bubble-sort.asm
@@ -3,33 +3,33 @@ ARRAY: .WORD 5, 3, 6, 8, 9, 1, 4, 7, 2, 10
 
 .TEXT
 BUBBLE:
-    LI      $T0,        0                   # I = 0
-    LI      $T1,        0                   # J = 0
-    LI      $S0,        10                  # SIZE = 10
-    LI      $S1,        ARRAY               # ADDRESS OF ARRAY.
+    LI          $T0,            0                           # I = 0
+    LI          $T1,            0                           # J = 0
+    LI          $S0,            10                          # SIZE = 10
+    LI          $S1,            ARRAY                       # ADDRESS OF ARRAY.
 OUTERFOR:
-    BLE     $S0,        $T0,    END         # IF SIZE <= I, EXIT OUTERFOR.
-    ADDI    $T0,        $T0,    1           # I = I + 1
-    LI      $T1,        0                   # J = 0
-    J       INNERFOR
+    BLE         $S0,            $T0,        END             # IF SIZE <= I, EXIT OUTERFOR.
+    ADDI        $T0,            $T0,        1               # I = I + 1
+    LI          $T1,            0                           # J = 0
+    J           INNERFOR
 INNERFOR:
-    BLE     $S0,        $T1,    OUTERFOR    $ IF SIZE <= J, RESTART OUTERFOR.
-    LI      $S2,        $S1                 # $S2 = ARRAY
-    ADD     $S2,        $S2,    $T0         # $S2 = ARRAY + J
-    LI      $S3,        $S1,                # $S3 = ARRAY
-    ADD     $S3,        $S3,    $T0         # $S3 = ARRAY + J
-    ADDI    $S3,        1                   # $S3 = ARRAY + J + 1
-    BEQ     ($S2),      ($S3)   SWAP        # IF (ARRAY[J] > ARRAY[J + 1])
-    ADDI    $T1,        $T1,    1           # J = J + 1
-    J       INNERFOR
+    BLE         $S0,            $T1,        OUTERFOR        # IF SIZE <= J, RESTART OUTERFOR.
+    LI          $S2,            $S1                         # $S2 = ARRAY
+    ADD         $S2,            $S2,        $T0             # $S2 = ARRAY + J
+    LI          $S3,            $S1,                        # $S3 = ARRAY
+    ADD         $S3,            $S3,        $T0             # $S3 = ARRAY + J
+    ADDI        $S3,            1                           # $S3 = ARRAY + J + 1
+    BEQ         ($S2),          ($S3)       SWAP            # IF (ARRAY[J] > ARRAY[J + 1])
+    ADDI        $T1,            $T1,        1               # J = J + 1
+    J           INNERFOR
 END:
     SYSCALL
 
 SWAP:
-    LI      $S4,        $S2                 # TEMP = ARRAY + J
-    LI      $S2,        $S3                 # ARRAY + J = ARRAY + J + 1
-    LI      $S3,        $S4                 # ARRAY + J + 1 = TEMP
-    J       $RA                             # RETURN TO INNERFOR.
+    LI          $S4,        $S2                             # TEMP = ARRAY + J
+    LI          $S2,        $S3                             # ARRAY + J = ARRAY + J + 1
+    LI          $S3,        $S4                             # ARRAY + J + 1 = TEMP
+    J           $RA                                         # RETURN TO INNERFOR.
 
 # void bubble(int array[], int size) {
 #     int temp = 0;
