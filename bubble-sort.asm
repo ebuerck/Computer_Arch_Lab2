101947124b8b6977500922cf4c25bd1b191559da
Bobby Love <lovebobbyandrew@outlook.com>
2022-10-17 10:59:20 -0500

    bubble sort wip

--- bubble-sort.asm
+++ bubble-sort.asm
@@ -6,6 +6,7 @@ BUBBLE:
     LI      $T0,        0                   # I = 0
     LI      $T1,        0                   # J = 0
     LI      $S0,        10                  # SIZE = 10
+    LI      $S1,        ARRAY               # ADDRESS OF ARRAY.
 OUTERFOR:
     BLE     $S0,        $T0,    END         # IF SIZE <= I, EXIT OUTERFOR.
     ADDI    $T0,        $T0,    1           # I = I + 1
@@ -13,10 +14,19 @@ OUTERFOR:
     J       INNERFOR
 INNERFOR:
     BLE     $S0,        $T1,    OUTERFOR    $ IF SIZE <= J, RESTART OUTERFOR.
-
+    LI      $S2,        $S1                 # $S2 = ARRAY
+    ADD     $S2,        $S2,    $T0         # $S2 = $S2 + J
+    LI      $S3,        $S1,                # $S3 = ARRAY
+    ADD     $S3,        $S3,    $T0         # $S3 = $S3 + J
+    ADDI    $S3,        1                   # $S3 = $S3 + 1
+    BEQ     ($S2),      ($S3)   SWAP        # IF (ARRAY[J] > ARRAY[J + 1])
     ADDI    $T1,        $T1,    1           # J = J + 1
     J       INNERFOR
 END:
+
+SWAP:
+
+
 
 # void bubble_sort(int a[], int n) {
 #     int tmp = 0;
